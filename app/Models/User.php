cb23b3f1a7a74146e47922ffc23f99abed8153be
malilovsobir92 <malilovsobir92@gmail.com>
2023-03-15 18:54:54 +0500

    Added companies relationship

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,4 +42,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * Get the user companies.
+     */
+    public function companies()
+    {
+        return $this->hasMany(Company::class);
+    }
 }

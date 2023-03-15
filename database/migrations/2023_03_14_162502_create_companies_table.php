1413b8073561f231117be25ebb1fae0153339ced
malilovsobir92 <malilovsobir92@gmail.com>
2023-03-15 18:49:26 +0500

    Change companies column

---
+++ database/migrations/2023_03_14_162502_create_companies_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('owner_fname');
+            $table->string('email')->unique();
+			$table->string('address');
+			$table->string('website');
+			$table->string('phone')->unique();
+            $table->unsignedBigInteger('user_id')->constrained('users')->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('companies');
+    }
+};

95697f62af06d5fae5b4471399dfee5614c78489
malilovsobir92 <malilovsobir92@gmail.com>
2023-03-15 18:55:46 +0500

    Added company relationship and fillable

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -8,4 +8,29 @@ use Illuminate\Database\Eloquent\Model;
 class Employee extends Model
 {
     use HasFactory;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'passport_pin',
+        'surname',
+        'name',
+        'middle_name',
+        'position',
+        'phone',
+        'address',
+        'company_id'
+    ];
+
+    protected $dates = [
+        'created_at',
+        'updated_at',
+    ];
+
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }
 }

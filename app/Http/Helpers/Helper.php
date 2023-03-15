9bedca53ba17daabb25b75b725b6d890acf02ce9
malilovsobir92 <malilovsobir92@gmail.com>
2023-03-15 18:51:28 +0500

    Validate token function

--- app/Http/Helpers/Helper.php
+++ app/Http/Helpers/Helper.php
@@ -2,15 +2,29 @@
 namespace App\Http\Helpers;
 
 use Illuminate\Http\Exceptions\HttpResponseException;
+use Laravel\Sanctum\PersonalAccessToken;
 
 class Helper{
     public static function sendError($message, $errors = [], $code = 401)
     {
         $response = ['success' => false, 'message' => $message];
         if (!empty($errors)) {
-            $response['date'] = $errors;
+            $response['data'] = $errors;
         }
 
         throw new HttpResponseException(response()->json($response, $code)); 
+    }
+
+    public static function getUserByToken($token = '')
+    {
+        $user = [];
+        if (!empty($token)) {
+            $checkToken = PersonalAccessToken::findToken($token);
+            if ($checkToken) {
+                $user = $checkToken->tokenable;
+            }
+        }
+
+        return $user;
     }
 }

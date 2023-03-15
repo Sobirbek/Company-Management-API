f121489e6702ed618c6951ae00d06e2e4179ec19
malilovsobir92 <malilovsobir92@gmail.com>
2023-03-15 18:55:16 +0500

    Added employee seeder

---
+++ database/seeders/EmployeeSeeder.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Carbon\Carbon;
+
+class EmployeeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('employees')->insert([
+            [
+                'passport_pin' => Str::random(10),
+                'surname' => Str::random(5),
+                'name' => Str::random(5).'@gmail.com',
+                'middle_name' => Str::random(5).' '.Str::random(5),
+                'address' => Str::random(5).' street USA',
+                'position' => Str::random(8),
+                'phone' => '99893'.rand(1000000, 10000000),
+                'company_id' => 1,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+            [
+                'passport_pin' => Str::random(10),
+                'surname' => Str::random(5),
+                'name' => Str::random(5).'@gmail.com',
+                'middle_name' => Str::random(5).' '.Str::random(5),
+                'address' => Str::random(5).' street USA',
+                'position' => Str::random(8),
+                'phone' => '99893'.rand(1000000, 10000000),
+                'company_id' => 1,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+            [
+                'passport_pin' => Str::random(10),
+                'surname' => Str::random(5),
+                'name' => Str::random(5).'@gmail.com',
+                'middle_name' => Str::random(5).' '.Str::random(5),
+                'address' => Str::random(5).' street USA',
+                'position' => Str::random(8),
+                'phone' => '99893'.rand(1000000, 10000000),
+                'company_id' => 2,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+            [
+                'passport_pin' => Str::random(10),
+                'surname' => Str::random(5),
+                'name' => Str::random(5).'@gmail.com',
+                'middle_name' => Str::random(5).' '.Str::random(5),
+                'address' => Str::random(5).' street USA',
+                'position' => Str::random(8),
+                'phone' => '99893'.rand(1000000, 10000000),
+                'company_id' => 1,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+        ]);
+    }
+}

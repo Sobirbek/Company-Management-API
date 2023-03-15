ebe6b640118e7765f92520cbd5360c23ce3d6353
malilovsobir92 <malilovsobir92@gmail.com>
2023-03-15 18:54:28 +0500

    Optimized route

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\CompanyController;
+use App\Http\Controllers\Api\EmployeeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,9 +17,14 @@ use App\Http\Controllers\Api\AuthController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+Route::apiResource('companies', CompanyController::class)->except(['show']);
+Route::apiResource('employees', EmployeeController::class)->except(['index', 'create', 'show', 'store']);
+Route::get('companies/{company}/employees', [EmployeeController::class, 'getEmployeesByCompanyId']);
+Route::post('companies/{company}/employees', [EmployeeController::class, 'store']);
 
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
+

b2c1725a514fc7a2a93444f723eaf3974ae047fe
malilovsobir92 <malilovsobir92@gmail.com>
2023-03-15 18:48:03 +0500

    Fixed auth token problem

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -6,35 +6,29 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
 use App\Http\Helpers\Helper;
-use Auth;
 use App\Http\Resources\UserResource;
 use Laravel\Sanctum\PersonalAccessToken;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Role;
+use App\Models\User;
+use Auth;
 
 class AuthController extends Controller
 {
     public function register(RegisterRequest $request)
     {
-        //Get request token
-        $token = request()->bearerToken();
-        if (empty($token)) {
-            Helper::sendError(__('Token is empty!'));
-        }
-        //Check token
-        $checkToken = PersonalAccessToken::findToken($token);
-        if (!$checkToken) {
-            Helper::sendError(__('Token is incorrect!'));
-        }
-
-        // $user = $checkToken->tokenable;
-        // $role = $user->getRoleNames();
-        // if (!in_array($role, array('admin'))) {
-        //     Helper::sendError(__('You do not have permissions to use the API'));
-        // }
-
         //Register new user
-
-
-        return true;
+        $user = User::create([
+            'name'      => $request->name,
+            'email'     => $request->email,
+            'password'  => Hash::make($request->password)
+        ]);
+        $role = Role::where(['name' => 'company'])->first();
+        if ($role) {
+            $user->assignRole($role);
+        }
+        
+        return new UserResource($user);
     }
 
     public function login(LoginRequest $request)

614c7fe1ac5d83fbb4d5e5e925f1aebedbc2eb4e
malilovsobir92 <malilovsobir92@gmail.com>
2023-03-21 15:55:02 +0500

    Added auth request

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -4,39 +4,42 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginRequest;
-use App\Http\Requests\RegisterRequest;
+use App\Http\Requests\AuthRequest;
+use Illuminate\Http\Request;
 use App\Http\Helpers\Helper;
 use App\Http\Resources\UserResource;
 use Laravel\Sanctum\PersonalAccessToken;
-use Illuminate\Support\Facades\Hash;
 use Spatie\Permission\Models\Role;
 use App\Models\User;
 use Auth;
 
 class AuthController extends Controller
 {
-    public function register(RegisterRequest $request)
+    //Register new user
+    public function register(AuthRequest $request)
     {
-        //Register new user
-        $user = User::create([
-            'name'      => $request->name,
-            'email'     => $request->email,
-            'password'  => Hash::make($request->password)
-        ]);
+        $user = User::create($request->all());
         $role = Role::where(['name' => 'company'])->first();
         if ($role) {
             $user->assignRole($role);
         }
-        
         return new UserResource($user);
     }
 
-    public function login(LoginRequest $request)
+    //Login
+    public function login(AuthRequest $request)
     {
         if (!Auth::attempt($request->only('email', 'password'))) {
             Helper::sendError(__('Email or Password is wrong!'));
         }
 
         return new UserResource(Auth()->user());
+    }
+
+    //Logout
+    public function logout()
+    {
+        auth()->user()->tokens()->delete();
+        return response(['message' => __('Logged out') ], 200);
     }
 }

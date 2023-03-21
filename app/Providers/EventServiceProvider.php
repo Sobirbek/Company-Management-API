e6374878d287a80a92b0d8051d0a05cad8675625
malilovsobir92 <malilovsobir92@gmail.com>
2023-03-21 16:00:55 +0500

    Added company observer to boot

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -6,6 +6,8 @@ use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Event;
+use App\Models\Company;
+use App\Observers\CompanyObserver;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -25,7 +27,7 @@ class EventServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Company::observe(CompanyObserver::class);
     }
 
     /**
